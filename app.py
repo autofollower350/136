2364d82de086af098bf0a0b0c9c4c32a594a8ddf
autofollower350 <followerauto350@gmail.com>
2025-07-06 17:31:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.binary_location = "/opt/chrome/chrome"
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(service=Service("/usr/local/bin/chromedriver"), options=options)
+driver.get("https://www.google.com")
+print("✅ Opened:", driver.title)
+driver.quit()
---
+++ startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Chrome v136 डाउनलोड करें
+wget -q https://storage.googleapis.com/chrome-for-testing-public/136.0.7103.113/linux64/chrome-linux64.zip
+unzip -q chrome-linux64.zip
+mv chrome-linux64 /opt/chrome
+ln -sf /opt/chrome/chrome /usr/bin/google-chrome
+
+# ChromeDriver v136 डाउनलोड करें
+rm -f /usr/local/bin/chromedriver
+wget -q -O chromedriver.zip https://storage.googleapis.com/chrome-for-testing-public/136.0.7103.113/linux64/chromedriver-linux64.zip
+unzip -o chromedriver.zip
+mv chromedriver-linux64/chromedriver /usr/local/bin/chromedriver
+chmod +x /usr/local/bin/chromedriver

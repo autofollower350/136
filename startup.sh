ed428638872cabc23081e48e23e1b83dadd03021
autofollower350 <followerauto350@gmail.com>
2025-07-07 13:04:36 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -10,9 +10,9 @@ ln -sf /opt/chrome/chrome /usr/bin/google-chrome
 
 # Install ChromeDriver
 wget -q -O chromedriver.zip https://storage.googleapis.com/chrome-for-testing-public/136.0.7103.113/linux64/chromedriver-linux64.zip
-unzip -q -o chromedriver.zip
-mv chromedriver-linux64/chromedriver /opt/chromedriver
-chmod +x /opt/chromedriver
+unzip -q chromedriver.zip
+mv chromedriver-linux64/chromedriver /usr/bin/chromedriver
+chmod +x /usr/bin/chromedriver
 
-echo "✅ Chrome and ChromeDriver installed"
-ls -l /opt/chromedriver
+echo "✅ Chrome + ChromeDriver installed"
+ls -l /usr/bin/chromedriver

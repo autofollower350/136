c4f4830f16c5fa7acf5f16675e7d38beb1c67595
autofollower350 <followerauto350@gmail.com>
2025-07-07 13:22:36 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,13 +2,13 @@
 
 echo "📦 Installing Chrome and ChromeDriver..."
 
-# Install Chrome
+# Install Chrome (v136)
 wget -q https://storage.googleapis.com/chrome-for-testing-public/136.0.7103.113/linux64/chrome-linux64.zip
 unzip -q chrome-linux64.zip
 mv chrome-linux64 /opt/chrome
 ln -sf /opt/chrome/chrome /usr/bin/google-chrome
 
-# Install ChromeDriver
+# Install matching ChromeDriver (v136)
 wget -q -O chromedriver.zip https://storage.googleapis.com/chrome-for-testing-public/136.0.7103.113/linux64/chromedriver-linux64.zip
 unzip -q chromedriver.zip
 mv chromedriver-linux64/chromedriver /usr/bin/chromedriver

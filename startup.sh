7bb8e3ccc82204f3a147768e0c51f4188278ea66
autofollower350 <followerauto350@gmail.com>
2025-07-07 14:08:36 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,21 +5,21 @@ pip install selenium
 
 echo "📦 Installing Chrome and ChromeDriver..."
 
-# Create local bin dirs
-mkdir -p /tmp/chrome-bin
-mkdir -p /tmp/chromedriver-bin
+# Create local bin dirs inside project dir
+mkdir -p .chrome
+mkdir -p .chromedriver
 
 # Download and extract Chrome
 wget -q https://storage.googleapis.com/chrome-for-testing-public/136.0.7103.113/linux64/chrome-linux64.zip
 unzip -q chrome-linux64.zip
-mv chrome-linux64/chrome /tmp/chrome-bin/chrome
+mv chrome-linux64/chrome .chrome/chrome
 
 # Download and extract ChromeDriver
 wget -q -O chromedriver.zip https://storage.googleapis.com/chrome-for-testing-public/136.0.7103.113/linux64/chromedriver-linux64.zip
 unzip -q chromedriver.zip
-mv chromedriver-linux64/chromedriver /tmp/chromedriver-bin/chromedriver
-chmod +x /tmp/chromedriver-bin/chromedriver
+mv chromedriver-linux64/chromedriver .chromedriver/chromedriver
+chmod +x .chromedriver/chromedriver
 
-echo "✅ Chrome and ChromeDriver installed to /tmp"
-ls -l /tmp/chrome-bin/chrome
-ls -l /tmp/chromedriver-bin/chromedriver
+echo "✅ Chrome and ChromeDriver installed to project folder"
+ls -l .chrome/chrome
+ls -l .chromedriver/chromedriver
